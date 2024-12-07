55a041faadc4c416654fe7bd18146e55cc5303ac
Cristian Mesaros <Cristian-Ioan.MESAROS@ext.ec.europa.eu>
2024-12-08 00:59:18 +0100

    Laborator 8 terminat

--- MesarosCristianLab7/App.xaml.cs
+++ MesarosCristianLab7/App.xaml.cs
@@ -1,7 +1,22 @@
-﻿namespace MesarosCristianLab7
+﻿
+using MesarosCristianLab7.Data;
+namespace MesarosCristianLab7
 {
     public partial class App : Application
     {
+        static ShoppingListDatabase database;
+        public static ShoppingListDatabase Database
+        {
+            get
+            {
+                if (database == null)
+                {
+                    database = new ShoppingListDatabase(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.
+                    LocalApplicationData), "ShoppingList.db3"));
+                }
+                return database;
+            }
+        }
         public App()
         {
             InitializeComponent();
