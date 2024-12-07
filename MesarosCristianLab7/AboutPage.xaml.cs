97abae1e5d1ff9aa8239b15c1225c33c43f3e568
Cristian Mesaros <Cristian-Ioan.MESAROS@ext.ec.europa.eu>
2024-12-07 22:38:57 +0100

    Add project files.

---
+++ MesarosCristianLab7/AboutPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace MesarosCristianLab7;
+
+public partial class AboutPage : ContentPage
+{
+	public AboutPage()
+	{
+		InitializeComponent();
+	}
+}
---
+++ MesarosCristianLab7/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MesarosCristianLab7
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
